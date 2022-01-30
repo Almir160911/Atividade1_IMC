fba4feb67bc8de3a44f8cb8071c922cc6907de5e
almir160911 <fernandesalmir31@gmail.com>
2022-01-29 23:58:52 -0300

    primeiro commit

--- imc.js
+++ imc.js
@@ -12,17 +12,17 @@ function imc () {
         let classificacao = '';
 
         if (valorIMC < 18.5){
-            classificacao = 'abaixo do peso.';
+            classificacao = 'Abaixo do peso.';
         }else if (valorIMC < 25) {
-            classificacao = 'com peso ideal. Parabéns!!!';
+            classificacao = 'Com peso ideal. Parabéns!!!';
         }else if (valorIMC < 30){
-            classificacao = 'levemente acima do peso.';
+            classificacao = 'Levemente acima do peso.';
         }else if (valorIMC < 35){
-            classificacao = 'com obesidade grau I.';
+            classificacao = 'Com obesidade grau I.';
         }else if (valorIMC < 40){
-            classificacao = 'com obesidade grau II';
+            classificacao = 'Com obesidade grau II';
         }else {
-            classificacao = 'com obesidade grau III. Cuidado!!';
+            classificacao = 'Com obesidade grau III. Cuidado!!';
         }
 
         resultado.textContent = `Seu IMC é ${valorIMC} e você está ${classificacao}`;
